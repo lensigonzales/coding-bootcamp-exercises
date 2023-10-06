a5988c39074d95e92abf1b1d980baade9d530cd2
lensigonzales <lena.si@outlook.com>
2023-10-06 15:50:55 +0200

    refactor: remove parse to array

--- coding-foundation-katas-main/zip-strings/zip-strings.js
+++ coding-foundation-katas-main/zip-strings/zip-strings.js
@@ -3,15 +3,13 @@ function zipStrings(strA, strB) {
   if (strA.length < 2 || strB.length === 0) {
     return strA.concat(strB);
   }
-  //parse both strings to arrays
-  const arrayA = strA.split("");
-  const arrayB = strB.split("");
+
   //get the longest length of the two arrays
   const longestLength = Math.max(strA.length, strB.length);
   //create a new Array with the longest length of the two strings
   let result = Array(longestLength).fill();
   //iterate over the new array and add elements of first and second array alternately
-  result = result.map((el, index) => [arrayA[index], arrayB[index]]);
+  result = result.map((el, index) => [strA[index], strB[index]]);
   //parse the array to a string and remove the commas and return
   return result.join(",").replace(/,/g, "");
 }
