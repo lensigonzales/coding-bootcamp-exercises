c302a52b3ae4c24bbe1044e77b76d6bfc338abe5
lensigonzales <lena.si@outlook.com>
2023-10-10 16:02:38 +0200

    add password toggle coding challenge

---
+++ web-apps-foundation/password-toggle/script.js
@@ -0,0 +1,16 @@
+"use strict";
+//change input type to text
+//change button label to "Hide Password"
+
+const inputField = document.querySelector(".input-pwd");
+const btn = document.querySelector(".toggle-btn");
+console.log(btn);
+btn.addEventListener("click", (event) => {
+  if (inputField.type === "password") {
+    inputField.type = "text";
+    btn.innerText = "Hide Password";
+  } else {
+    inputField.type = "password";
+    btn.innerText = "Show Password";
+  }
+});
